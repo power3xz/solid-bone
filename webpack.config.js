8ada4804474191c1502513d9c33d378a78d9bb05
devj <power3xz@gmail.com>
2023-05-01 14:29:05 +0900

    webpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-
 module.exports = {
   mode: 'development',
-  entry: ['./src/index.ts'],
-
-  // output JS bundle to: build/bundle.js
+  entry: ['./src/index.tsx'],
   output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'bundle.js',
+    filename: '[id]-[contenthash].js',
+    chunkFilename: '[id]-[contenthash].[ext]',
+    path: path.resolve(__dirname, './dist'),
   },
-
-  plugins: [new HtmlWebpackPlugin()],
-  devtool: 'source-map',
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  resolve: { extensions: ['.tsx', '.ts', '.js', '.jsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   devServer: {
     port: 8888,
   },
