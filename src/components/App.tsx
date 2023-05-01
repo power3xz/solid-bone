9cbc9979b24a5623c812917eb5b52e648e1b35fb
devj <power3xz@gmail.com>
2023-05-01 19:22:55 +0900

    변수명 수정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,22 @@
 import { Component } from 'solid-js';
-import style from './App.module.scss';
+
+import styles from './App.module.scss';
 import { Content } from './Content';
 import { Footer } from './Footer';
 import { Header } from './Header';
+
 export const App: Component = () => {
   return (
-    <div class={style.app}>
-      <div class={style.header}>
+    <div class={styles.app}>
+      <div class={styles.header}>
         <Header />
       </div>
 
-      <div class={style.content}>
+      <div class={styles.content}>
         <Content />
       </div>
 
-      <div class={style.footer}>
+      <div class={styles.footer}>
         <Footer />
       </div>
     </div>
