133cf7341556ce5f1c037159b4af58ded569d636
devj <power3xz@gmail.com>
2023-05-01 19:22:21 +0900

    classnames 추가

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,14 @@
+import { Component, createSignal } from 'solid-js';
+import cs from 'classnames/bind';
 import styles from './Logo.module.scss';
+const cx = cs.bind(styles);
 
-export const Logo = () => <div class={styles.logo}></div>;
+export const Logo: Component = () => {
+  const [isOn, setIsOn] = createSignal(false);
+  return (
+    <div
+      class={cx('logo', { isOn: isOn() })}
+      onClick={() => setIsOn(!isOn())}
+    ></div>
+  );
+};
