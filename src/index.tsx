2f21db559e94972c21457ff317f12442bdcaae08
devj <power3xz@gmail.com>
2023-05-01 17:19:09 +0900

    global.scss 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { render } from 'solid-js/web';
 import App from './components/App';
 import './reset.scss';
+import './global.scss';
 
 render(() => <App />, document.querySelector('#root') as HTMLElement);
