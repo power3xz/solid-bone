33cda66c61ea030f39adddc6c3c8c7a5421e72df
devj <power3xz@gmail.com>
2023-05-01 13:50:24 +0900

    웹팩설정

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/index.ts"],
+
+  // output JS bundle to: build/bundle.js
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "bundle.js",
+  },
+
+  plugins: [new HtmlWebpackPlugin()],
+  devtool: "source-map",
+  devServer: {
+    port: 8888,
+  },
+};
