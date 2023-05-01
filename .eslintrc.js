3b63fbf4c423be234d05b8e8240c755a776ad2d6
devj <power3xz@gmail.com>
2023-05-01 13:58:02 +0900

    eslint prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,22 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  files: ['src/**/*'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: ["./src/index.ts"],
+  mode: 'development',
+  entry: ['./src/index.ts'],
 
   // output JS bundle to: build/bundle.js
   output: {
-    path: path.resolve(__dirname, "./build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
   },
 
   plugins: [new HtmlWebpackPlugin()],
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
     port: 8888,
   },
